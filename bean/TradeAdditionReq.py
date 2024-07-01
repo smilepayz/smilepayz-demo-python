24967ae07a06d19b30d57a772d5d7888f38c3dd5
moore <moore@tradev-group.com>
2024-07-01 14:42:30 +0800

    commit

--- bean/TradeAdditionReq.py
+++ bean/TradeAdditionReq.py
@@ -2,17 +2,15 @@ class TradeAdditionReq:
     # ifscode: Only for India Pay out to Bank ,11 digits (Mandatory)
     # taxNumber : Only for Brazil pay out , which method is CPF/CNPJ ,this is tax number for CPF/CNPJ
     # payerAccountNo : Only for Thailand pay in which method is BANK,this is means your paying bank account no
-    # network : for usdt
-    def __init__(self, ifscCode, taxNumber, payerAccountNo, network):
+    def __init__(self, ifscCode, taxNumber, payerAccountNo):
         if ifscCode is not None:
             self.ifscCode = ifscCode
         if taxNumber is not None:
             self.taxNumber = taxNumber
         if payerAccountNo is not None:
             self.payerAccountNo = payerAccountNo
-        if network is not None:
-            self.network = network
+
 
 
     def print_info(self):
-        print(f" {self.ifscCode} {self.taxNumber} {self.payerAccountNo} {self.network}")
+        print(f" {self.ifscCode} {self.taxNumber} {self.payerAccountNo}")
