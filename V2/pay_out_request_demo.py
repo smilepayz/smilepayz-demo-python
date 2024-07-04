828d973108721c6d049d373eac97ae48841df5b3
moore <moore@tradev-group.com>
2024-07-04 17:13:22 +0800

    merchant_secret

--- V2/pay_out_request_demo.py
+++ V2/pay_out_request_demo.py
@@ -14,35 +14,28 @@ from bean.PayerReq import PayerReq
 from bean.ReceiverReq import ReceiverReq
 from bean.TradeAdditionReq import TradeAdditionReq
 from bean.TradePayoutReq import TradePayoutReq
-
-# from step2
-
-private_key_str = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC3LbmfPhjGXJ+a6NVKyRWEfCpsKfl9UFnMRltNosJv+7qd6OUK3t7Q8fKX8rxuJBeXLLWrOZvjoGlP3ybvvhUSGKH+BLcN1k2eJbcKuThvXvKQvd7pXolY91gkF8V78FX+TKegZupbeji0XUGXCBNyShocYM6Cvailf0Iyv49dcktJmi4drBKfgj5l4HUY9TaDuCZOrhvFZfZZxBK1zkm72k0ZLfqTGmG8O2tsByndCTH2aPLT9odR8/O4qfoTG+vV9HivIKKOI2h0kZfQBOqWD4/ofwu9PNoRRzGgzfRpF3GhnKa3bRVSpCUuBtHfotQfFxbjaDk0s3K5BhQE7HxzAgMBAAECggEARHhaBxUeC595pVzcxVyOp3wGG3JBKL9NIZc277kj9tngcsAoRTzziqS1qmh4WK8zBjYXHg6ln5tJYiqmkjy6AY6llp7KkeiGENRGLEL5vl9+Se4/EXpd2pxyHOOp1N8MNccPbVyqw1DXO0wUhVDme/UI94yUBLjB/kKoSvHhs+qwJ8cz9C8sF3Zs/zF6Te/f+Z+HrIbVj4vlx6DYBs3pWe7J+XYg3XXpbvIBJVTN0lreQAjtopic0F7o1EULnllJmOqy+kiRMuSi5ESrWFOnuCrY0iz/C8LJKlqWa3d+1jVLPvKYucXvddYrwjyu8kYHZAenKWKkLPlQkQxGUFcX8QKBgQDYSCsgg7tDlXTw1P9Z2BhkVo1ugYWA1FcxqTY2sTOMOwCTt3QZAFEqofbNvdjZbk7vu8gh5LyxbWAr/sNqOXH5915a6DZKBGl+qBTH2TZZKmhCUWlmn8T7ySYclfsgIZsUxHaDfY+otiXGtNegSX9US9/AFVSMdiQBCmr5/i48mQKBgQDY0U5+MDl6GnbDH/hCT+YMq/m5W58m7ehJfG6jPtLQkPPvMq2Oj5HUeXx4vDdUxzaRlC9fICwe2KgKiGYpJsRPte6JfO6te7wsO2lDk0oBiw+jewm4CwZ5KeDYyReha8Hc2H1j5hllVx5DIZiage2ZswS5+kCNgzf4QbdAOd886wKBgAo5TyCYWY/WTtLbnr6GgpCrrr/ci40NfJmyYAex1Lf6Sgqxj2FnLG8RfPM42DlfB4g5njpL78eLXhJ2VpJ86LBiSymM9JQHJV2BYIoZ8IHCiW8pHgxl3Q/x8EVFqbtZG1Wd++Q3WUUmZx6/ibnf/47ij08rMvX417bc4TW0GEdxAoGAPX3nUBynQH0e76oyg8QbT766nZphoe3ZcnYK/tuDeMmTlWR/Gq6XQnaOGcPvwWiajmFDqiv6t2jlB8+1gbhP9vd3RqEbJDKypKzY5uRwGc3xyoDLudnOpTB+Z51oyUxBeDwiG+IXk8lIeOufVzrAQ1YlYgWap0fu6MbijSGcsa8CgYEA0OolBxRKdBSxmwMbXDocg5HtHd15UgJdlgL9GFjDZPisUvM7LWK97E5QPhY2IEIcv0EW9jM62bc1uPMhwDuN1dzmLafQIivGsIj5qUy18CoE4KJHsTeKLeHkrd6/nBd4da4A37+ksJ1t/sgZpjl40ShrxTV+OtzzY/kuQ4Uqc9g="
-public_key_str = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAty25nz4YxlyfmujVSskVhHwqbCn5fVBZzEZbTaLCb/u6nejlCt7e0PHyl/K8biQXlyy1qzmb46BpT98m774VEhih/gS3DdZNniW3Crk4b17ykL3e6V6JWPdYJBfFe/BV/kynoGbqW3o4tF1BlwgTckoaHGDOgr2opX9CMr+PXXJLSZouHawSn4I+ZeB1GPU2g7gmTq4bxWX2WcQStc5Ju9pNGS36kxphvDtrbAcp3Qkx9mjy0/aHUfPzuKn6Exvr1fR4ryCijiNodJGX0ATqlg+P6H8LvTzaEUcxoM30aRdxoZymt20VUqQlLgbR36LUHxcW42g5NLNyuQYUBOx8cwIDAQAB"
-merchant_secret_sandbox = "6a58a603e5043290f4097ee4a7745661b3656932d4eebc3106b5dddc3af6e053"
-merchant_secret = "95b57c46b8c2e068982be23fb669a80612cad68e6ce6ba4f5af9ec20d23bb274"
-merchant_id_sandbox = "sandbox-20019"
-merchant_id = "20019"
+from bean.Constants import Constants
 
 
 def transaction_pay_in():
-    print("=====>  Payout disbursement")
+    # production
+    merchant_id = Constants.merchantId
+    merchant_secret = Constants.merchantSecret
+    request_path = Constants.baseUrl + "/v2.0/disbursement/pay-out"
 
-    # url
-    path_url = "https://gateway.smilepayz.com/v2.0/disbursement/pay-out"
-    path_url_sandbox = "https://sandbox-gateway.smilepayz.com/v2.0/disbursement/pay-out"
+    # sandbox
+    # merchant_id = Constants.merchantIdSandBox
+    # merchant_secret = Constants.merchantSecretSandBox
+    # request_path = Constants.baseUrlSandbox + "/v2.0/disbursement/pay-out"
+
 
     # transaction time
     timestamp = Tool_Sign.get_formatted_datetime('Asia/Bangkok')
     print("timestamp:" + timestamp)
     # partner_id
-    merchant_order_no = "T_" + str(time.time())
+    merchant_order_no = merchant_id + Tool_Sign.generate_32bit_uuid()
     purpose = "Purpose For Transaction from python SDK"
 
-    # https://docs-test.smilepayz.com/en/reference/version1.0/data/paymentMethod_Brazil.html
-    # https://docs-test.smilepayz.com/en/reference/version1.0/data/paymentMethod_India.html
-    # https://docs-test.smilepayz.com/en/reference/version1.0/data/paymentMethod_Indonesia.html
-    # https://docs-test.smilepayz.com/en/reference/version1.0/data/paymentMethod_Thailand.html
     payment_method = "YES"  # bank code  for india
     cashAccount = "1234566778"  # cah account for Yes bank
     # moneyReq
@@ -68,7 +61,7 @@ def transaction_pay_in():
                                   "14450", "82-3473233732", "Indonesia")
     addition_req = TradeAdditionReq("YES000001", None, None);
     # payInReq,  None fields are optional
-    pay_in_req = TradePayoutReq(payment_method, None, None, cashAccount, merchant_order_no, purpose,
+    pay_in_req = TradePayoutReq(payment_method, None, None, cashAccount, merchant_order_no[:32], purpose,
                                 None,
                                 addition_req,
                                 None, None, None, money_req, merchant_req, None,
@@ -79,11 +72,12 @@ def transaction_pay_in():
     print("json_data_minify=", json_data_minify)
 
     # build
-    string_to_sign =  timestamp + "|" + merchant_secret + "|" + json_data_minify
+    string_to_sign = timestamp + "|" + merchant_secret + "|" + json_data_minify
     print("string_to_sign=", string_to_sign)
+    print("request_path=", request_path)
 
     # signature
-    signature = Tool_Sign.sha256RsaSignature(private_key_str, string_to_sign)
+    signature = Tool_Sign.sha256RsaSignature(Constants.privateKeyStr, string_to_sign)
     print("signature=", signature)
 
     # post
@@ -96,7 +90,7 @@ def transaction_pay_in():
 
     }
     # POST request
-    response = requests.post(path_url, data=json_data_minify, headers=headers)
+    response = requests.post(request_path, data=json_data_minify, headers=headers)
     # Get response result
     result = response.json()
     print(result)
