e078a9a0b70399db6a7663272854244330dfd98e
Sonia <sonia@tradev-group.com>
2025-06-23 15:31:01 +0800

    init work on sonia_test branch

--- v2/philippines/bean/Constants.py
+++ v2/philippines/bean/Constants.py
@@ -5,6 +5,6 @@ class Constants:
 
     baseUrlSandbox = "https://sandbox-gateway.smilepayz.com"
 
-    baseUrl = "https://gateway.smilepayz.com"
+    baseUrl = "https://gateway-test.smilepayz.com"
 
 
